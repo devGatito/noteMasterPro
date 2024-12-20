fa9fcacff02ea31c38014446daa2dd118b024d17
devGatito <eliasmisaelsotojimenez1@gmail.com>
2024-12-20 14:17:43 -0600

    creacion de navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,27 @@
-
+"use client";
+import { useState, useEffect } from "react";
+import SplashScreen from "@/components/SplashScreen";
+import Dashboard from "@/dashboard/page";
+import NavBar from "@/components/NavBar";
 export default function Home() {
+  const [showContent, setShowContent] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setShowContent(true), 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
+      {!showContent && <SplashScreen />}
+      {showContent && (
+
+        <>
+        <NavBar/>
+        <Dashboard/>
+        
+        </>
+      )}
     </>
-    
   );
 }
---
+++ components/SplashScreen.tsx
@@ -0,0 +1,27 @@
+"use client";
+import { useEffect, useState } from "react";
+
+export default function SplashScreen() {
+  const [showSplash, setShowSplash] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setShowSplash(false), 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (showSplash) {
+    return (
+      <div className="flex items-center justify-center h-screen ">
+        <div className="text-center">
+          {/* <Image src="/icon.png" alt="logoScreen" width={200} height={300} />*/}
+          <h1 className="text-4xl font-bold mt-4">NoteMaster Pro</h1>
+          <p className="text-lg text-gray-400 mt-2">
+            Tu espacio digital para crear y colaborar...
+          </p>
+        </div>
+      </div>
+    );
+  }
+
+  return null;
+}
